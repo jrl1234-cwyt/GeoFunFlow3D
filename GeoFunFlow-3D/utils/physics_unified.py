c1fd0ef43d0261f963e4f4332bafa1f864e0f85e
jrl1234-cwyt <jrl489166@gmail.com>
2026-04-17 10:54:36 -0400

    Update physics_unified.py

--- GeoFunFlow-3D/utils/physics_unified.py
+++ GeoFunFlow-3D/utils/physics_unified.py
@@ -6,11 +6,9 @@ import torch.nn.functional as F
 class SurfaceAerodynamicsPhysics(nn.Module):
     def __init__(self):
         super().__init__()
-        # 四阶中心差分模板
         self.c1, self.c2, self.c3, self.c4 = 1.0 / 12.0, -8.0 / 12.0, 8.0 / 12.0, -1.0 / 12.0
 
     def _get_latent_gradients(self, field):
-        """仅计算潜空间的离散网格差分"""
         padded_x = F.pad(field, (2, 2, 0, 0, 0, 0))
         grad_x = (self.c1 * padded_x[..., :-4] + self.c2 * padded_x[..., 1:-3] +
                   self.c3 * padded_x[..., 3:-1] + self.c4 * padded_x[..., 4:])
@@ -24,7 +22,6 @@ class SurfaceAerodynamicsPhysics(nn.Module):
 
     def forward(self, field_grid, mask, radius_xyz, normals=None):
         gx_lat, gy_lat, gz_lat = self._get_latent_gradients(field_grid)
-        # 🚀 物理数学推导: ∂ϕ/∂x_real = (∂ϕ/∂x_lat) / R_x
         rx = radius_xyz[:, 0].view(-1, 1, 1, 1, 1) + 1e-6
         ry = radius_xyz[:, 1].view(-1, 1, 1, 1, 1) + 1e-6
         rz = radius_xyz[:, 2].view(-1, 1, 1, 1, 1) + 1e-6
@@ -41,7 +38,6 @@ class VolumeThermodynamicsPhysics(nn.Module):
         self.R, self.gamma = 287.0, 1.4
 
     def _gradient(self, f):
-        """返回潜空间的 x,y,z 离散梯度"""
         fx = (F.pad(f, (1, 1, 0, 0, 0, 0), mode='replicate')[..., 2:] - F.pad(f, (1, 1, 0, 0, 0, 0), mode='replicate')[
             ..., :-2]) * 0.5
         fy = (F.pad(f, (0, 0, 1, 1, 0, 0), mode='replicate')[..., 2:, :] -
@@ -54,7 +50,6 @@ class VolumeThermodynamicsPhysics(nn.Module):
         pred_real = pred_norm * self.data_std + self.data_mean
         p, rho, t = pred_real[:, 0:1], pred_real[:, 1:2], pred_real[:, 2:3]
 
-        # 🚀 【新增】：物理量绝对安全铠甲，防止负数被求分数次方导致 NaN！
         p = torch.abs(p) + 1e-4
         rho = torch.abs(rho) + 1e-4
         t = torch.abs(t) + 1e-4
@@ -63,10 +58,9 @@ class VolumeThermodynamicsPhysics(nn.Module):
         ry = radius_xyz[:, 1].view(-1, 1, 1, 1, 1) + 1e-6
         rz = radius_xyz[:, 2].view(-1, 1, 1, 1, 1) + 1e-6
 
-        # 1. EOS
+
         loss_eos = (p - rho * self.R * t).abs().mean() / 1e5
 
-        # 2. 真实物理梯度
         gpx_lat, gpy_lat, gpz_lat = self._gradient(p)
         gpx, gpy, gpz = gpx_lat / rx, gpy_lat / ry, gpz_lat / rz
 
@@ -78,7 +72,6 @@ class VolumeThermodynamicsPhysics(nn.Module):
         gsx_lat, gsy_lat, gsz_lat = self._gradient(s)
         gsx, gsy, gsz = gsx_lat / rx, gsy_lat / ry, gsz_lat / rz
 
-        # 3. 熵约束
         loss_isentropic = ((gsx.abs() + gsy.abs() + gsz.abs()) * smooth_mask).mean() / 1e3
         entropy_prod = gpx * gsx + gpy * gsy + gpz * gsz
         loss_second_law = (F.relu(-entropy_prod) * shock_mask).mean() / 1e6
